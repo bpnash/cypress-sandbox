4d49c479de4ef63afeb97660ce5248959c080db3
bpnash <bpnash65@gmail.com>
2023-05-25 08:24:32 +0100

    Added todo tests for completed todos

--- cypress/e2e/todo-mvc/todo.cy.js
+++ cypress/e2e/todo-mvc/todo.cy.js
@@ -1,55 +1,87 @@
-describe("example to-do app", () => {
+describe('example to-do app', () => {
   beforeEach(() => {
-    cy.visit("http://todomvc-app-for-testing.surge.sh/");
+    cy.visit('http://todomvc-app-for-testing.surge.sh/');
   });
 
-  it("Page heading by class & tag is todos", () => {
-    cy.get(".header > h1").should("contain", "todos");
+  it('Page heading by class & tag is todos', () => {
+    cy.get('.header > h1').should('contain', 'todos');
   });
 
-  it("Page heading by tag is todos", () => {
-    cy.get("h1").should("contain", "todos");
+  it('Page heading by tag is todos', () => {
+    cy.get('h1').should('contain', 'todos');
   });
 
-  it("Input should contain placeholder text", () => {
-    cy.get(".header > input")
-      .invoke("attr", "placeholder")
-      .should("contain", "What needs to be done?");
+  it('Input should contain placeholder text', () => {
+    cy.get('.header > input')
+      .invoke('attr', 'placeholder')
+      .should('contain', 'What needs to be done?');
   });
 
   // Remove hard coded values
-  it("Entering a todo entry updates the value attribute", () => {
-    cy.get(".header > input")
-      .type("Buy milk, eggs & sugar")
-      .invoke("attr", "value")
-      .should("contain", "Buy milk, eggs & sugar");
+  it('Entering a todo entry updates the value attribute', () => {
+    cy.get('.header > input')
+      .type('Buy milk, eggs & sugar')
+      .invoke('attr', 'value')
+      .should('contain', 'Buy milk, eggs & sugar');
   });
 
-  it("Entering a todo entry should persist", () => {
-    cy.get(".header > input").type("Buy milk, eggs & sugar{enter}");
-    cy.get("label").should("contain", "Buy milk, eggs & sugar");
+  it('Entering a todo entry should persist', () => {
+    cy.get('.header > input').type('Buy milk, eggs & sugar{enter}');
+    cy.get('label').should('contain', 'Buy milk, eggs & sugar');
   });
 
-  it("Entering multiple todo entries should persist", () => {
-    cy.get(".header > input").type("Buy milk, eggs & sugar{enter}");
-    cy.get(".header > input").type("Collect dry cleaning{enter}");
-    cy.get("label").should("have.length", 2);
-    cy.get("label").eq(0).should("contain", "Collect dry cleaning");
-    cy.get("label").eq(1).should("contain", "Buy milk, eggs & sugar");
+  it('Entering multiple todo entries should persist', () => {
+    cy.get('.header > input').type('Buy milk, eggs & sugar{enter}');
+    cy.get('.header > input').type('Collect dry cleaning{enter}');
+    cy.get('label').should('have.length', 2);
+    cy.get('label').eq(0).should('contain', 'Collect dry cleaning');
+    cy.get('label').eq(1).should('contain', 'Buy milk, eggs & sugar');
   });
 
-  it("Edit an existing todo should persist");
+  // This test took a couple of hours to write 😖 but got there in the end 💪
+  it('Edit an existing todo should persist', () => {
+    cy.get('.header > input').type('Buy milk, eggs & sugar{enter}');
+    cy.get('label').should('contain', 'Buy milk, eggs & sugar');
+    cy.get('label').should('have.length', 1);
+    cy.get('label').dblclick();
+    // element get detached from the DOM at this point so continuing the command chain fails
+    // The field with a <label> task gets re rendered and becomes an <input> again
+    cy.get('.editing > input').type(' & cheese{enter}');
+    // Same thing happens here the element is re rendered as a <label>
+    cy.get('label').should('contain', 'Buy milk, eggs & sugar & cheese');
+  });
 
-  it("Completing a task displays as strikethrough");
+  it.only('Completing a task displays as strikethrough', () => {
+    const task1 = 'Todo test one';
+    const task2 = 'Todo test two';
+    cy.get('.header > input')
+      .type(task1 + '{enter}')
+      .type(task2 + '{enter}');
+    cy.get('label').should('have.length', 2);
+    // don't need `footer` but is useful as an indicator to know where it is
+    cy.get('footer > span.todo-count').should('contain', '2');
+    // check the second todo using find
+    cy.get('.todo-list > li').eq(1).find('input.toggle').click();
+    // assertions
+    cy.get('.todo-list > li.completed').should('exist');
+    cy.get('footer > span.todo-count').should('contain', '1');
+    // these are the same 1st is neater
+    cy.get('.todo-list > li.completed').find('label').should('contain', task1);
+    cy.get('.todo-list > li.completed label').should('contain', task1);
+    // Finding the CSS property
+    cy.get('.todo-list > li.completed label')
+      .invoke('css', 'text-decoration')
+      .should('contain', 'line-through');
+  });
 
-  it("Active tasks shows only active todos");
+  it('Active tasks shows only active todos');
 
-  it("completed tasks shows only completed todos");
+  it('completed tasks shows only completed todos');
 
-  it("Number of active tasks displays correctly");
+  it('Number of active tasks displays correctly');
 
   // create tasks that show on all 3 filters and delete them
-  it("Deleting tasks removes tasks from all lists");
+  it('Deleting tasks removes tasks from all lists');
 
   // check through the advanced examples and use variations of ALL of them if possible
 });
